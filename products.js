1219d3ff516bbcd4f3edd14d3f57e55be471cf31
Abbas <rizviabbas128@gmail.com>
2023-02-18 18:26:06 +0530

    Added Shopping Cart

---
+++ products.js
@@ -0,0 +1,59 @@
+import product1 from './assets/iphone.jpg';
+import Product2 from './assets/laptop.jpg';
+import product3 from './assets/camera.jpg';
+import product4 from './assets/protein.webp';
+import product5 from './assets/sweater.jpg';
+import product6 from './assets/earphone.webp';
+import product7 from './assets/shoes1.jpg';
+import product8 from './assets/shirt.jpg';
+
+export const PRODUCTS = [
+    {
+        id:1,
+        productName: "Iphone",
+        price: 120000,
+        productImage:product1,
+    },
+    {
+        id:2,
+        productName: "Asus Tuf a15",
+        price: 80000,
+        productImage: Product2,
+    },
+    {
+        id:3,
+        productName: "Nikkon",
+        price: 275000,
+        productImage:product3,
+    },
+    {
+        id:4,
+        productName: "ON Whey Protein",
+        price: 3800,
+        productImage:product4,
+    },
+    {
+        id:5,
+        productName: "H & M Slim Fit Sweater",
+        price: 1500,
+        productImage: product5
+    },
+    {
+        id:6,
+        productName: "Boat rockerz 235 V2",
+        price: 5500,
+        productImage: product6,
+    },
+    {
+        id:7,
+        productName: "Zara Canvas low ankle",
+        price: 4700,
+        productImage: product7,
+    },
+    {
+        id:8,
+        productName: "Men Maroon Cotton Solid Slim Fit Shirt",
+        price: 1200,
+        productImage: product8,
+    }
+];
